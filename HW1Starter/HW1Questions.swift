a04cc4bf50db0ab32174a2f0ef9533e26c48dd5a
Leahpol <leapol234@berkeley.edu>
2025-09-10 19:57:59 -0700

    done

--- HW1Starter/HW1Questions.swift
+++ HW1Starter/HW1Questions.swift
@@ -17,7 +17,15 @@ class HW1Questions {
     ///   - count: Target length of file name (excluding the file type)
     /// - Returns: An array of file names whose excluded file type length matches `count`.
     func getFileNames(for filenames: [String], withCount count: Int) -> [String] {
-        return []
+        var result = [String]()
+        var name = String()
+        for str in filenames {
+            name = String(str.split(separator: ".", maxSplits: 1)[0])
+            if name.count == count {
+                result += [str]
+            }
+        }
+        return result
     }
     
     
@@ -38,6 +46,22 @@ class HW1Questions {
     ///   - escapeIndex: The ending index
     /// - Returns: A boolean. True if we can escape. False otherwise.
     func canEscape(withDirections directions: [[Direction]], startingIndex: Int, escapeIndex: Int) -> Bool {
+        for instruction in directions{
+            var i = startingIndex
+            for direction in instruction {
+                if direction == .right {
+                    i = i + 1
+                } else if direction == .left{
+                    i = i - 1
+                } else {
+                    i = startingIndex
+                    break
+                }
+            }
+            if i == escapeIndex {
+                return true
+            }
+        }
         return false
     }
     
